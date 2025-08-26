0247f47c355e9dfc40a3aa34deb1d00e9d9df6ea
anyasauce <anyajb014@gmail.com>
2025-08-26 11:05:54 +0800

    update

--- server/app/routes/lostFoundRoutes.js
+++ server/app/routes/lostFoundRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { fetchItems, addItem, updateStatus, deleteItem  } from '../controllers/lostFoundController.js';
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get('/lost-found-items', fetchItems);
-router.post('/add-lost-found-item', addItem);
+router.post('/add-lost-found-item', upload.single("image"), addItem);
 router.put('/update-status/:id', updateStatus);
 router.delete('/delete-item/:id', deleteItem);
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import session from 'express-session';
+import path from 'path';
+
 import authRoutes from './app/routes/authRoutes.js';
 import lostFoundRoutes from './app/routes/lostFoundRoutes.js';
 import serviceRequestRoutes from './app/routes/serviceRequestRoutes.js';
@@ -12,12 +14,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Session middleware
 app.use(session({
@@ -27,6 +31,9 @@ app.use(session({
     cookie: { secure: false, httpOnly: true }
 }));
 
+app.use('/uploads', express.static('uploads'));
+
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/lostfound', lostFoundRoutes);
 app.use('/api/servicerequest', serviceRequestRoutes);
